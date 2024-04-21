b6aa61cd1d055818d4db8392d1ae4f2794125710
Kunal Jadhav <154059626+Itsbitscoder@users.noreply.github.com>
2024-04-22 00:30:40 +0530

    src update

--- src/data/constants.js
+++ src/data/constants.js
@@ -300,16 +300,17 @@ export const education = [
 export const projects = [
   {
     id: 1,
-    title: "Vexa",
+    title: "portfolio",
     date: "Oct 2022 - Present",
     description:
-      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
-    image:
-      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+      "A portfolio can refer to a collection of academic and professional materials that demonstrates a person's skills, qualifications, education, training, and experiences.",
+    image:"https://cache.careers360.mobi/media/article_images/2022/4/20/How-to-make-portfolio-for-design-admission.webp",
     tags: [
+
       "Html",
       "CSS",
-      "JavaScript"
+      "JavaScript",
+      "React.js"
     ],
     category: "web app",
     github: "https://github.com/rishavchanda/Project-Management-App",
@@ -370,5 +371,4 @@ export const TimeLineData = [
   { year: 2018, text: "Worked as a freelance developer" },
   { year: 2019, text: "Founded JavaScript Mastery" },
   { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
 ];
